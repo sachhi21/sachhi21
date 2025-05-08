1c64c1d1317375b78b9e8fada0630a63ac19fdcf
Sachin Rampur <99388531+sachhi21@users.noreply.github.com>
2025-05-09 02:07:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
-        chunkSizeWarningLimit: 100000000
-    },
-    base: "/sachhi21",
-})
+     base: '/sachin.io/', 
+
+});
